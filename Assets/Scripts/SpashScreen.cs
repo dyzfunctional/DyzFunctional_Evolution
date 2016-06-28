b88523089f2b8731930c70448fca1faff197a881
dyzfunctional <dyzfunctionalstudio@cableone.net>
2016-06-28 13:14:29 -0600

    Added Splash Screen
    Added splash Screen and Script

---
+++ Assets/Scripts/SpashScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SpashScreen : MonoBehaviour
+{
+    public float timer = 2f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(DisplayScene());
+	}
+
+    IEnumerator DisplayScene()
+    {
+        yield return new WaitForSeconds(timer);
+
+        SceneManager.LoadScene(1);
+    }
+}
